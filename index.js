2bfcc6a16597b3b98b388e0a4a45d184071a252c
Дмитрий Павлов <sarville@gmail.com>
2022-12-08 12:16:58 +0300

    learning git

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 'use strict';
+{
+  const productName = 'Чай';
+  const productCategory = 'Бакалея';
+  let productCost = 100;
+  let productQuantity = 50;
+  
+  console.log(productName);
+  console.log('Общая стоимость: ' + productCost * productQuantity + 'р.');
+}
+
+{
+  const productName = 'Печенье';
+  const productCategory = 'Бакалея';
+  let productCost = 55;
+  let productQuantity = 150;
+  
+  console.log(productName);
+  console.log('Общая стоимость: ' + productCost * productQuantity + 'р.');
+}
