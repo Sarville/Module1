8189c17ff6a200b983d7c6f25d9cd696aa6d76ba
Дмитрий Павлов <sarville@gmail.com>
2022-12-08 11:08:53 +0300

    initial

---
+++ index.js
@@ -0,0 +1,7 @@
+const productName = 'Чай';
+const productCategory = 'Бакалея';
+let productCost = 100;
+let productQuantity = 50;
+
+console.log(productName);
+console.log('Общая стоимость: ' + productCost * productQuantity + 'р.');
