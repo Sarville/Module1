8eafe81edd02a8b8e2d2bcc4db047c90e3da98a0
Дмитрий Павлов <sarville@gmail.com>
2023-01-07 20:42:58 +0300

    topic11

--- topic10/cart.js
+++ topic10/cart.js
@@ -1,47 +1,50 @@
 'use strict';
 {
- let cart = {
-  items: [],
-  get totalPrice() {
-    return this.calculateItemPrice();
-  },
-  count: 0,
-  
-  add(name, price, qty = 1) {
-    this.items.push({name, price, qty});
-    this.increaseCount(qty);
-  },
+  const cart = {
+    items: [],
+    get totalPrice() {
+      return this.calculateItemPrice();
+    },
+    count: 0,
 
-  increaseCount(q) {this.count += q},
+    add(name, price, qty = 1) {
+      this.items.push({name, price, qty});
+      this.increaseCount(qty);
+    },
 
-  calculateItemPrice() {
-    let totalPrice = 0;
-    this.items.forEach((item) => {totalPrice += item.price * item.qty});
-    return totalPrice;
-  },
+    increaseCount(q) {
+      this.count += q;
+    },
 
-  clear() {
-    this.items = [];
-    this.totalPrice = 0;
-    this.count = 0;
-  },
+    calculateItemPrice() {
+      let totalPrice = 0;
+      this.items.forEach((item) => {
+        totalPrice += item.price * item.qty;
+      });
+      return totalPrice;
+    },
 
-  print() {
-    console.log(JSON.stringify(this.items));
-    console.log(this.totalPrice);
-  },
- }
- cart.add('apple', 100);
- cart.add('coca-cola', 200, 5);
- cart.add('tea', 50, 1);
+    clear() {
+      this.items = [];
+      this.totalPrice = 0;
+      this.count = 0;
+    },
 
- cart.print();
+    print() {
+      console.log(JSON.stringify(this.items));
+      console.log(this.totalPrice);
+    },
+  };
+  cart.add('apple', 100);
+  cart.add('coca-cola', 200, 5);
+  cart.add('tea', 50, 1);
 
-//  проверка других методов
+  cart.print();
 
-//  cart.increaseCount(10);
-//  console.log(cart.count);
-//  cart.clear();
-//  console.log(cart);
+  //  проверка других методов
 
+  //  cart.increaseCount(10);
+  //  console.log(cart.count);
+  //  cart.clear();
+  //  console.log(cart);
 }
